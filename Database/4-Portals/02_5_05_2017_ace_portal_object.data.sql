7f4052f38fffc7de0e48a439aca954c55969336e
Ty Conner <tyconner@itcproductions.com>
2017-05-06 18:27:15 -0400

    Fixing error in  script

--- Database/4-Portals/02_5_05_2017_ace_portal_object.data.sql
+++ Database/4-Portals/02_5_05_2017_ace_portal_object.data.sql
@@ -5,7 +5,7 @@ USE `ace_world`;
 --
 
 INSERT INTO `ace_portal_object` (`weenieClassId`, `destLandblockId`, `destX`, `destY`, `destZ`, `destQX`, `destQY`, `destQZ`, `destQW`, `min_lvl`, `max_lvl`, `societyId`, `isTieable`, `isRecallable`, `isSummonable`) VALUES
-(7934, 48300688, 310, -260, -11.995, 0, 0, 0, -0.707, -0.707, 25, 0, 1, 1, 0),
+(7934, 48300688, 310, -260, -11.995, 0, 0, -0.707, -0.707, 25, 0, 0, 1, 1, 0),
 (8190, 2558263312, 60, -60, 0.005, 0, 0, 1, 0, 150, 0, 0, 1, 1, 0),
 (9071, 393936960, 180.1, 188.343, 560.005, 0, 0, -1, -0.004, 0, 0, 0, 1, 1, 0),
 (27298, 1699218044, 90, -80, 0.005, 0, 0, 0, 1, 90, 0, 0, 1, 1, 1),
