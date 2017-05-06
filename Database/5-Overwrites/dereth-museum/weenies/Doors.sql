fc3b1733c6088cb11399703020425012cc778c05
Ty Conner <tyconner@itcproductions.com>
2017-05-06 18:38:38 -0400

    Release 0.0.4 (#6)
    * Version 0.0.4
    
    * Restructured folders.
    * New sorting of object files.
    * Refined object filters to eliminate more not static objects.
    
    * Updating changelog.md
    
    * Fixed an error in syntax for update script
    
    * Fixing error in  script

---
+++ Database/5-Overwrites/dereth-museum/weenies/Doors.sql
@@ -0,0 +1,6 @@
+REPLACE INTO `base_ace_object` (`baseAceObjectId`, `name`, `typeId`, `paletteId`, `ammoType`, `blipColor`, `bitField`, `burden`, `combatUse`, `cooldownDuration`, `cooldownId`, `effects`, `containersCapacity`, `header`, `hookTypeId`, `iconId`, `iconOverlayId`, `iconUnderlayId`, `hookItemTypes`, `itemsCapacity`, `location`, `materialType`, `maxStackSize`, `maxStructure`, `radar`, `pscript`, `spellId`, `stackSize`, `structure`, `targetTypeId`, `usability`, `useRadius`, `validLocations`, `value`, `workmanship`, `animationFrameId`, `defaultScript`, `defaultScriptIntensity`, `elasticity`, `friction`, `locationId`, `modelTableId`, `objectScale`, `physicsBitField`, `physicsTableId`, `motionTableId`, `soundTableId`, `physicsState`, `translucency`, `currentMotionState`)
+VALUES (412, 'Door', 128, 0, 0, 0, 4116, 0, 0, 0, 0, 0, 0, 48, 0, 100668183, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 33561087, 0, 104451, 872415275, 150995458, 536870947, 65544, 0, 'AAA9AAIAAAAMAAAA');
+
+REPLACE INTO weenie_class (`weenieClassId`, `baseAceObjectId`)
+VALUES (412, 412);
+
--- changelog.md
+++ changelog.md
@@ -1,5 +1,9 @@
 # ACE-World Change Log
 
+### 2017-05-06
+[Ripley]
+* Restructured scripts.
+
 ### 2017-04-26
 [Ripley]
 * Added initial SQL insert scripts for world population.
