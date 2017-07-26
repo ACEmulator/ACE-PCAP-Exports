5a64cc70f84ba92347de538c6588caad168be3ad
Ty Conner <tyconner@itcproductions.com>
2017-07-26 12:38:56 -0400

    Delete all core files (#30)
    This is to fix issues with the repo

--- Database/3-Core/2-weenieapprasialobjects/BooksShardVigil_0.sql
+++
@@ -1,6 +0,0 @@
-REPLACE INTO `ace_object_properties_int` (`aceObjectId`, `intPropertyId`, `propertyValue`)
-VALUES (9002, 19, 0)
-     , (9002, 5, 10000)
-     , (9002, 174, 9)
-     , (9002, 175, 9);
-
--- Database/3-Core/4-apprasialobjects/BooksShardVigil_0.sql
+++
@@ -1,6 +0,0 @@
-REPLACE INTO `ace_object_properties_int` (`aceObjectId`, `intPropertyId`, `propertyValue`)
-VALUES (2880118656, 19, 0)
-     , (2880118656, 5, 10000)
-     , (2880118656, 174, 9)
-     , (2880118656, 175, 9);
-
