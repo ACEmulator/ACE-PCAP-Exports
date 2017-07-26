ce857baacbe241025953736057ca271eb38bf7c4
Ty Conner <tyconner@itcproductions.com>
2017-07-26 12:38:18 -0400

    Delete all core files
    This is to fix issues with the repo

--- Database/3-Core/2-weenieapprasialobjects/HousePortals_0.sql
+++
@@ -1,6 +0,0 @@
-REPLACE INTO `ace_object_properties_string` (`aceObjectId`, `strPropertyId`, `propertyValue`)
-VALUES (11730, 38, 'House Portal');
-
-REPLACE INTO `ace_object_properties_int` (`aceObjectId`, `intPropertyId`, `propertyValue`)
-VALUES (11730, 111, 49);
-
