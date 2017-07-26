f24f69000e99f78b5d0d5fab2b9bda75c695de70
Ty Conner <tyconner@itcproductions.com>
2017-07-26 12:42:30 -0400

    Restore apprasial objects

---
+++ Database/3-Core/4-apprasialobjects/CreaturesSpecialNPCs_0.sql
@@ -0,0 +1,30 @@
+REPLACE INTO `ace_object_properties_int` (`aceObjectId`, `intPropertyId`, `propertyValue`)
+VALUES (3688325439, 2, 11)
+     , (3688325439, 134, 16)
+     , (3688325439, 25, 10497);
+
+REPLACE INTO `ace_object_properties_attribute` (`aceObjectId`, `attributeId`, `attributeBase`)
+VALUES (3688325439, 1, 1055)
+     , (3688325439, 2, 6000)
+     , (3688325439, 4, 50)
+     , (3688325439, 3, 50)
+     , (3688325439, 5, 50)
+     , (3688325439, 6, 30);
+
+REPLACE INTO `ace_object_properties_attribute2nd` (`aceObjectId`, `attribute2ndId`, `attribute2ndValue`)
+VALUES (3688325439, 2, 3200)
+     , (3688325439, 4, 6200)
+     , (3688325439, 6, 30)
+     , (3688325439, 1, 3200)
+     , (3688325439, 3, 6200)
+     , (3688325439, 5, 30);
+
+REPLACE INTO `ace_object_properties_int` (`aceObjectId`, `intPropertyId`, `propertyValue`)
+VALUES (3688325438, 2, 28)
+     , (3688325438, 134, 16)
+     , (3688325438, 25, 1334);
+
+REPLACE INTO `ace_object_properties_attribute2nd` (`aceObjectId`, `attribute2ndId`, `attribute2ndValue`)
+VALUES (3688325438, 2, 900)
+     , (3688325438, 1, 900);
+
