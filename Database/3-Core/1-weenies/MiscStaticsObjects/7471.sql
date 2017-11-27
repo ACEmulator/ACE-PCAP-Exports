a71d9845bcbebf631e193b2f6b2c8e21a097c614
Ty Conner <tyconner@itcproductions.com>
2017-11-27 04:49:11 -0500

    Remove some data duplicated

--- Database/3-Core/1-weenies/MiscStaticsObjects/7471.sql
+++
@@ -1,6 +0,0 @@
-/* Extended Appraisal Data */
-
-REPLACE INTO `ace_object_properties_int` (`aceObjectId`, `intPropertyId`, `propertyValue`)
-VALUES (7471, 19, 1) /* VALUE_INT */
-     , (7471, 5, 1) /* ENCUMB_VAL_INT */;
-
